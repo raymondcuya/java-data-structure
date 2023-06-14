2e75abc712fbb1c0487d2fdaed74dfc58bb1132f
raymond cuya <raymondcuya@gmail.com>
2023-06-14 22:00:23 +1000

    initialise array and print

---
+++ src/com/data/structure/linear/array/Array.java
@@ -0,0 +1,14 @@
+package com.data.structure.linear.array;
+
+public class Array {
+    private int[] items;
+    private int count;
+    public Array(int length) {
+        items = new int[length];
+    }
+
+    public void print() {
+        for (int i = 0; i < count; i++)
+            System.out.println(items[i]);
+    }
+}
---
+++ src/com/data/structure/linear/array/Main.java
@@ -0,0 +1,8 @@
+package com.data.structure.linear.array;
+
+public class Main {
+    public static void main(String[] args) {
+        Array numbers = new Array(3);
+        numbers.print();
+    }
+}
