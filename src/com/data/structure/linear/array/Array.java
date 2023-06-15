a4b7820315b9580eadd86bcf525041087a463cee
raymond cuya <raymondcuya@gmail.com>
2023-06-15 21:37:50 +1000

    array implementation of insert

--- src/com/data/structure/linear/array/Array.java
+++ src/com/data/structure/linear/array/Array.java
@@ -6,7 +6,17 @@ public class Array {
     public Array(int length) {
         items = new int[length];
     }
+    public void insert(int item) {
+        if (items.length == count) {
+            int[] newItems = new int[count*2];
 
+            for (int i = 0; i < count; i++)
+                newItems[i] = items[i];
+
+            items = newItems;
+        }
+        items[count++] = item;
+    }
     public void print() {
         for (int i = 0; i < count; i++)
             System.out.println(items[i]);
