363b50d518b2b62309b840c4dd67c76b1b12562e
raymond cuya <raymondcuya@gmail.com>
2023-06-22 21:37:48 +1000

    starting structure of linked list with node

---
+++ src/com/data/structure/linear/linkedlist/LinkedList.java
@@ -0,0 +1,6 @@
+package com.data.structure.linear.linkedlist;
+
+public class LinkedList {
+    private Node first;
+    private Node last;
+}
---
+++ src/com/data/structure/linear/linkedlist/Node.java
@@ -0,0 +1,6 @@
+package com.data.structure.linear.linkedlist;
+
+public class Node {
+    private int value;
+    private Node next;
+}
