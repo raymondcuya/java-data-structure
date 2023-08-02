6ad6725922ee4967666e72820b7cf40752ae98e5
raymond cuya <raymondcuya@gmail.com>
2023-08-02 23:19:26 +1000

    check if expression has open and close parenthesis

---
+++ src/com/data/structure/linear/stack/Expression.java
@@ -0,0 +1,21 @@
+package com.data.structure.linear.stack;
+
+import java.util.Stack;
+
+public class Expression {
+    public boolean isBalanced(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : input.toCharArray()) {
+            if(ch == '(')
+                stack.push(ch);
+
+            if (ch == ')') {
+                if (stack.empty()) return false;
+                stack.pop();
+            }
+        }
+
+        return stack.empty();
+    }
+}
