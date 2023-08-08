a2e12e342e059b2e07e6dfd951dc3a1980fbf2cf
raymond cuya <raymondcuya@gmail.com>
2023-08-08 22:59:42 +1000

    express is balance call

--- src/com/data/structure/linear/stack/Main.java
+++ src/com/data/structure/linear/stack/Main.java
@@ -4,6 +4,26 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
+//        demoJavaStack();
+//        demoStringReverse();
+        // Edge cases
+        // (
+        // (()
+        // ) (
+        String str = "(1 + 2)";
+        Expression exp = new Expression();
+        var result = exp.isBalanced(str);
+        System.out.println(result);
+    }
+
+    private static void demoStringReverse() {
+        String str = "abcd";
+        StringReverser reverser = new StringReverser();
+        var result = reverser.reverse(str);
+        System.out.println(result);
+    }
+
+    private static void demoJavaStack() {
         Stack<Integer> stack = new Stack<>();
         stack.push(10);
         stack.push(20);
